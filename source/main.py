e5666226350d0b347d647eb10f6332e787180470
Tie Ying Kathleen <tieyingkathleen@gmail.com>
2022-10-30 21:57:25 +0800

    Update main.py to read from both sample .csv files and gsheet links

--- source/main.py
+++ source/main.py
@@ -8,26 +8,51 @@ import holidays
 import datetime as dt
 from datetime import datetime, timedelta
 from storage import Month, Archive, Request
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-req_file_regex_c = "Call requests (Consultant) - Nov 2022.csv"
-req_file_regex_r = "Call requests (Reg) - Nov 2022.csv"
+
+cwd = os.getcwd()
+
 roster_archive_file_name = "Roster_Archive.xlsx"
 roster_period = 'Nov 2022'
 
-cwd = os.getcwd()
-input_c = pd.read_csv(os.path.join(cwd, 'data', req_file_regex_c), skiprows = 1)
-input_r = pd.read_csv(os.path.join(cwd, 'data', req_file_regex_r), skiprows = 1)
-
 roster_archive_c = pd.read_excel(os.path.join(cwd, 'data', roster_archive_file_name), sheet_name='Consultant', header=[0,1], index_col=[0])
 roster_archive_r = pd.read_excel(os.path.join(cwd, 'data', roster_archive_file_name), sheet_name='Reg', header=[0,1], index_col=[0])
-
 # Public holidays in Singapore
 sg_holidays = []
 for date in holidays.Singapore(years=datetime.strptime(roster_period, '%b %Y').year).items():
     sg_holidays.append(date[0])
 
-month_data = Month(roster_period, sg_holidays)
+roster_month = Month(roster_period, sg_holidays)
+
+input_type = input("Import from gsheet or folder: ")
+if (input_type == "folder"):
+    req_file_regex_c = "Call requests (Consultant) - Nov 2022.csv"
+    req_file_regex_r = "Call requests (Reg) - Nov 2022.csv"
+    input_c = pd.read_csv(os.path.join(cwd, 'data', req_file_regex_c), skiprows = 1)
+    input_r = pd.read_csv(os.path.join(cwd, 'data', req_file_regex_r), skiprows = 1)
+elif (input_type == "gsheet"):
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name(os.path.join(cwd,'source','passcode_key.json'), scope)
+    client = gspread.authorize(creds)
+
+    sheet_c = client.open('Call requests (Consultant)')
+    sheet_r = client.open('Call requests (Reg)')
+
+    sheet_instance_c = sheet_c.get_worksheet(0)
+    sheet_instance_r = sheet_r.get_worksheet(0)
+
+    sheet_name_c = sheet_instance_c.title
+    sheet_name_r = sheet_instance_r.title
+
+    input_c = pd.DataFrame.from_dict(sheet_instance_c.get_all_records(head=2))
+    input_r = pd.DataFrame.from_dict(sheet_instance_r.get_all_records(head=2))
+
 archive_c = Archive('Consultant', roster_archive_c)
 archive_r = Archive('Registra', roster_archive_r)
-request_c = Request(month_data, 'Consultant', input_c, False, False, archive_c.carryover_data)
-request_r = Request(month_data, 'Registra', input_c, False, False, archive_r.carryover_data)
+request_c = Request(roster_month, 'Consultant', input_c, False, False, archive_c.carryover_data)
+request_r = Request(roster_month, 'Registra', input_r, False, False, archive_r.carryover_data)
+
+request_c.print_details()
+request_r.print_details()
--- source/storage.py
+++ source/storage.py
@@ -15,7 +15,7 @@ class Month:
 
         self.roster_period = roster_period
         self.num_days = calendar.monthrange(start_datetime.year, start_datetime.month)[1]
-        end_datetime = start_datetime + timedelta(days=num_days-1)
+        end_datetime = start_datetime + timedelta(days=self.num_days-1)
 
         # Weekdays - Monday: 0, Tuesday: 1, Wednesday: 2, Thursday: 3, Friday: 4
         # Weekends - Saturday: 5, Sunday: 6
@@ -45,25 +45,23 @@ class Month:
 class Archive:
     def __init__(self, employee_type, raw_data):
         self.employee_type = employee_type
-        self.roster_list = self.raw_data['Name']
+        self.roster_list = raw_data.index
 
         raw_data.drop('Special_Days', axis=1, level=1, inplace=True)
         raw_data.drop('Normal_Days', axis=1, level=1, inplace=True)
         raw_data = raw_data.droplevel('Name', axis=1)
+        min_max = raw_data.agg(['min','max'])
+        mode = pd.DataFrame(raw_data.agg(lambda x: pd.Series.mode(x)[0])).transpose().rename({0: 'mode'})
 
-        self.min_max_mode = raw_data.agg(['min','max',lambda x: pd.Series.mode(x)[0]])
-        self.min_max_mode = self.min_max_mode.rename(index={'<lambda>': 'mode'})
-        
-        archive_data = pd.concat([raw_data, self.min_max_mode])
+        archive_data = pd.concat([raw_data, min_max, mode])
         carryover_data = archive_data.copy()
         carryover_data['Carry_Over'] = 0
         for col in archive_data.columns:
             # If employee cc score > mode, then result > 0, to be added to total_cc in model
             # If employee cc score < mode, then result < 0, to be deducted from total_cc in model
             carryover_data[col] = archive_data[col] - archive_data[col].loc['mode']
-            carryover_data['Carry_Over'] += archive_data[col]
+            carryover_data['Carry_Over'] += carryover_data[col]
             carryover_data['Carry_Over'] = carryover_data['Carry_Over'].fillna(0)
-        carryover_data['Name'] = self.roster_list
 
         self.archive_data = archive_data
         self.carryover_data = carryover_data
@@ -72,12 +70,12 @@ class Archive:
 class Request:
     def __init__(self, month, employee_type, raw_data, override_twoday, is_maxcalls, carryover_data):
         self.month = month
-        self.emp_type = employee_type
+        self.employee_type = employee_type
         self.raw_data = raw_data.fillna('')
 
         self.roster_list = self.raw_data['Name']
-        self.exc_list = self.raw_data.loc[self.raw_data['Exc']=='Yes','Name']
-        self.inc_list = self.raw_data.loc[self.roster_list.isin(self.exc_list)]
+        self.exc_list = self.raw_data.loc[self.raw_data['Ex']=='Yes','Name']
+        self.inc_list = self.roster_list.loc[~self.roster_list.isin(self.exc_list)]
         self.num_employees = len(self.roster_list)
         self.num_employees_exc = len(self.exc_list)
 
@@ -90,9 +88,11 @@ class Request:
         for emp_x in range(0, self.num_employees):  
             for day_x in range(0, self.month.num_days):
                 col = str(day_x+1)
+                #Add 0 in front of all days before day 10
+                if (day_x<9):
+                    col = "0" + str(day_x+1)
                 blocked_raw[emp_x, day_x] = (0 if self.raw_data.iloc[emp_x][col] in BLOCKED_TYPES else 1)
                 requested_raw[emp_x, day_x] = (1 if self.raw_data.iloc[emp_x][col] in REQUESTED_TYPES else 0)
-                                   
         # Employee blocked data
         blocked_data = pd.DataFrame(data = blocked_raw, columns = range(0, self.month.num_days), index = self.roster_list)
         self.blocked_data = blocked_data.stack().to_dict()
@@ -100,7 +100,7 @@ class Request:
         # Employee requested data
         requested_data = pd.DataFrame(data = requested_raw, columns = range(0, self.month.num_days), index = self.roster_list)
         self.requested_data = requested_data.stack().to_dict()
-
+        
         # Employee requested - 2 day constraint (yes/no)
         if (override_twoday):
             two_day_constraint_raw = self.raw_data['2D'].replace({'Yes': 0, 'No': 0}) # Set No for all employees to override original file input
@@ -110,14 +110,42 @@ class Request:
         self.twoday_data = two_day_constraint_data
 
         # Max calls for excluded employee
-        max_excluded_raw = self.raw_data.loc[self.raw_data['exc']=='Yes', 'Max']
+        max_excluded_raw = self.raw_data.loc[self.raw_data['Exc']=='Yes', 'Max']
         max_excluded_data = pd.DataFrame(data = max_excluded_raw.values, columns=['Max_Calls_Exc'], index=self.exc_list)
         self.max_data = max_excluded_data
 
         # Merge with archive carry over data
         # Extract carry-over values for all employees on current roster
-        carryover_data[carryover_data['Name'].isin(self.roster_list)]
-        self.carryover_data = carryover_data['Carry_Over']
+        carryover_data = raw_data.merge(carryover_data, left_on='Name', right_index=True, how='left')
+        carryover_data = carryover_data.set_index('Name')
+        carryover_data = carryover_data['Carry_Over']
+        self.carryover_data = carryover_data
+
+    def print_details(self):
+        
+        print("Roster period: " + self.month.roster_period + ", Employee type: " + self.employee_type)
+        
+        print("Raw data: ")
+        print(self.raw_data)
+
+        print("Roster list: ")
+        print(self.roster_list)
+
+        print("Excluded: ")
+        print(self.exc_list)
+
+        print("Included: ")
+        print(self.inc_list)
+
+        print("Two day data: ")
+        print(self.twoday_data)
+
+        print("Max data: ")
+        print(self.max_data)
+
+        print("Carry over data: ")
+        print(self.carryover_data)
+
 
     # TODO: Add methods to check for the feasibility of the request for:
     # (i) Daily feasibility: i.e. the day has at least one person
